9079218ff0150ab90a5ff61cd70fdf1656c37639
DonzKrieg <iromdhon2004@gmail.com>
2025-04-08 07:57:24 +0700

    menambahkan class TeamMembers103022330150

--- Program.cs
+++ Program.cs
@@ -6,5 +6,8 @@ public class Program
     {
         DataMahasiswa_103022330150 dataMahasiswa = new DataMahasiswa_103022330150();
         dataMahasiswa.ReadJSON();
+
+        TeamMembers_103022330150 teamMembers = new TeamMembers_103022330150();
+        teamMembers.ReadJSON();
     }
 }
---
+++ TeamMembers_103022330150.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static modul7_kelompok_5.DataMahasiswa_103022330150;
+
+namespace modul7_kelompok_5
+{
+    class TeamMembers_103022330150
+    {
+        public class Member
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string gender { get; set; }
+            public int age { get; set; }
+            public string nim { get; set; }
+        }
+
+        public class Group
+        {
+            public List<Member> members { get; set; }
+        }
+
+        public void ReadJSON()
+        {
+            string filePath = "C:/Konstruksi PL/modul7/modul7_kelompok_5/jurnal7_2_103022330150.json";
+
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                Group group = JsonSerializer.Deserialize<Group>(jsonString);
+                
+                Console.WriteLine($"Team member list:");
+                foreach (var member in group.members)
+                {
+                    Console.WriteLine($"{member.nim} {member.firstName} {member.lastName} {member.age} {member.gender}");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File JSON tidak ditemukan");
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("Format JSON tidak valid");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
