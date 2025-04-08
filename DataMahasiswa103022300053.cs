686d010cee0f1d0fb6f826e1f82d02c88070522e
TrianaJulianingsih <151098972+TrianaJulianingsih@users.noreply.github.com>
2025-04-08 08:07:14 +0700

    menambahkan class DataMahasiswa103022300053

---
+++ DataMahasiswa103022300053.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace modul7_kelompok_5
+{
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+    public class Mahasiswa
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public string age { get; set; }
+        public Address address { get; set; }
+        public List<Course> courses { get; set; }
+    }
+    class DataMahasiswa103022300053
+    {
+        public static void ReadJSON()
+        {
+            string filePath = "jurnal7_1_103022300053.json";
+            try {
+                string jsonString = File.ReadAllText(filePath);
+                Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+                Console.WriteLine($"Nama: {mhs.firstName}{mhs.lastName}");
+                Console.WriteLine($"Gender: {mhs.gender}");
+                Console.WriteLine($"Age: {mhs.age}");
+                Console.WriteLine($"Address: {mhs.address}");
+                Console.WriteLine($"Courses: ");
+                foreach (var course in mhs.courses)
+                {
+                    Console.WriteLine($" Code: {course.code}, Name: {course.name}");
+                }
+            }
+            catch(Exception ex){
+                Console.WriteLine($"Terjadi Error : {ex.Message}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace modul7_kelompok_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103022300053.ReadJSON();
+        }
+    }
+}
