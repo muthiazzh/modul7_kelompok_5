e0864dfccae8e5967abf248859fb1e7a5eb2c4d1
syahdan <syahdanmk1234@gmail.com>
2025-04-08 08:12:07 +0700

    Datamahasiswa

---
+++ DataMahasiswa103022300079.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa103022300079
+{
+    public string Nama { get; set; }
+    public string NIM { get; set; }
+    public string Kelas { get; set; }
+    public string MataKuliah { get; set; }
+
+    public static void ReadJSON()
+    {
+        string path = "jurnal7_1_103022300079.json";
+        string jsonString = File.ReadAllText(path);
+        var data = JsonSerializer.Deserialize<DataMahasiswa103022300079>(jsonString);
+        Console.WriteLine($"Nama: {data.Nama}, NIM: {data.NIM}, Kelas: {data.Kelas}, Mata Kuliah: {data.MataKuliah}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace jurnalmod7kpl_103022300079
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            DataMahasiswa103022300079.ReadJSON();
+        }
+    }
+}
