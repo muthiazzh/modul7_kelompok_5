39cae8080f099170d5831615821b4fb9997534eb
TrianaJulianingsih <151098972+TrianaJulianingsih@users.noreply.github.com>
2025-04-08 08:20:31 +0700

    menambahkan class TeamMembers103022300053

---
+++ TeamsMembers103022300053.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_5
+{
+    class TeamsMembers103022300053
+    {
+    }
+}
